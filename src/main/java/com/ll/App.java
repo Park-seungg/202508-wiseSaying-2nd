f997d031181912c682b953d10be5bac51ef39f55
Park-seungg <parkis1101@gmail.com>
2025-08-04 14:16:31 +0900

    초기설정

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,13 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
     void run () {
 
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+        scanner.nextLine();
+        System.out.println(cmd);
     }
 }

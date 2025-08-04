aed80f9713c4f393729284fe59b8128399098d87
Park-seungg <parkis1101@gmail.com>
2025-08-04 14:55:43 +0900

    TestUtil.getScanner - red

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
 rootProject.name = "wiseSaying2508-2nd"
-
---
+++ src/test/java/com/ll/standard/util/TestUtilTest.java
@@ -0,0 +1,14 @@
+package com.ll.standard.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class TestUtilTest {
+
+    @Test
+    @DisplayName("TestUtil.genScanner() 테스트")
+    public void t1() {
+        Scanner scanner = TestUtil.genScanner()
+    }

6dd41197affa200a9f6af28dcf52bd44839ab3db
Park-seungg <parkis1101@gmail.com>
2025-08-05 09:09:52 +0900

    등록기능 구현 - green

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -11,5 +11,23 @@ public class App {
 
     public void run () {
         System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.printf("명령) ");
+            String cmd = scanner.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.println("명언 : ");
+                    String content = scanner.nextLine();
+                    System.out.println("작가 : ");
+                    String author = scanner.nextLine();
+                }
+                case "종료" -> {
+                    System.out.println("앱을 종료합니다.");
+                    return;
+                }
+            }
+        }
     }
 }

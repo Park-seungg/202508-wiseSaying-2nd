6861619a70f8c9d8d9be107520e1d9d8ab2d98a5
Park-seungg <parkis1101@gmail.com>
2025-08-05 10:30:43 +0900

    목록 - blue

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -12,9 +12,9 @@ public class App {
     }
 
     public void run () {
-        int lastId = 0;
 
-        List<WiseSaying> wiseSayingList = new ArrayList<>();
+        SystemController systemcontroller = new SystemController();
+        WiseSayingController wiseSayingController = new WiseSayingController(scanner);
 
         System.out.println("== 명언 앱 ==");
 
@@ -24,27 +24,13 @@ public class App {
 
             switch (cmd) {
                 case "등록" -> {
-                    System.out.println("명언 : ");
-                    String content = scanner.nextLine();
-
-                    System.out.println("작가 : ");
-                    String author = scanner.nextLine();
-
-                    int id = ++lastId;
-
-                    wiseSayingList.add(new WiseSaying(id, author, content));
-
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    wiseSayingController.actionWrite();
                 }
                 case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-                    for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
-                        WiseSaying wiseSaying = wiseSayingList.get(i);
-                        System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent());
-                    }
+                    wiseSayingController.actionList();
                 }
                 case "종료" -> {
+                    systemcontroller.actionExit();
                     return;
                 }
             }
---
+++ src/main/java/com/ll/SystemController.java
@@ -0,0 +1,7 @@
+package com.ll;
+
+public class SystemController {
+    public void actionExit() {
+        System.out.println("== 명언 앱 종료 ==");
+    }
+}

2ae40980f9f4f8b37e8b2f6855ae44223ce5b56e
Park-seungg <parkis1101@gmail.com>
2025-08-05 09:36:48 +0900

    등록할때마다 생성되는 명언번호가 증가 - red

--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -9,7 +9,7 @@ public class AppTest {
 
     @Test
     @DisplayName("`== 명언 앱 == ` 출력")
-    public void t1 () {
+    public void t1() {
         String rs = AppTestRunner.run("");
         assertThat(rs).contains("== 명언 앱 ==");
     }
@@ -28,6 +28,7 @@ public class AppTest {
                 .contains("명언 : ")
                 .contains("작가 : ");
     }
+
     @Test
     @DisplayName("등록시 생성된 명언번호 노출")
     void t3() {
@@ -39,5 +40,22 @@ public class AppTest {
 
         assertThat(rs)
                 .contains("1번 명언이 등록되었습니다.");
+    }
+
+    @Test
+    @DisplayName("등록할때마다 생성되는 명언번호가 증가")
+    void t4() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.");
     }
 }

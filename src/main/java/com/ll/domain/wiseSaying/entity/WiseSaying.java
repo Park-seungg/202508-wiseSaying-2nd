080f0cb0fa1b03ced45cd6a42b9a92a243e69efc
Park-seungg <parkis1101@gmail.com>
2025-08-06 14:43:25 +0900

    JSON (2) - green

--- src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/ll/domain/wiseSaying/entity/WiseSaying.java
@@ -1,23 +1,44 @@
 package com.ll.domain.wiseSaying.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class WiseSaying {
     private int id;
     private String author;
     private String content;
 
-    public WiseSaying(String author, String content) {
+    public WiseSaying (String author, String content) {
         this.author = author;
         this.content = content;
     }
 
+    public WiseSaying(Map<String, Object> wiseSayingMap) {
+        this.id = (int) wiseSayingMap.get("id");
+        this.author = (String) wiseSayingMap.get("author");
+        this.content = (String) wiseSayingMap.get("content");
+    }
+
     public boolean isNew() {
         return id == 0;
+    }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new LinkedHashMap<>();
+
+        map.put("id", id);
+        map.put("author", author);
+        map.put("content", content);
+
+        return map;
     }
 }
